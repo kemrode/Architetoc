dbd034488dacae619ff51e3232dc93f1eef76cd3
kevin_fichou <kevinfichou@gmail.com>
2023-12-19 16:35:54 +0100

    feat(master): add SumOf

---
+++ source/Sum.js
@@ -0,0 +1,5 @@
+function SumOf(a, b) {
+    return a + b;
+};
+
+module.exports = SumOf;
---
+++ test/Sum.test.js
@@ -0,0 +1,5 @@
+const SumOf = require('../source/Sum');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(SumOf(1, 2)).toBe(3);
+});
